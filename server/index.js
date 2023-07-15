b32fee20121a7849e692717629afd6f76b50f1e4
文茁 <wen.zhuo@belle.com.cn>
2023-07-15 22:53:50 +0800

    week2

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList');
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+// TODO: hardcode a merkle root here representing the whole nice list
+const merkleTree = new MerkleTree(niceList);
+// paste the hex string in here, without the 0x prefix
+const MERKLE_ROOT = merkleTree.getRoot();;
+
+app.post('/gift', (req, res) => {
+  // grab the parameters from the front-end here
+  const body = req.body;
+  var name = body.name;
+  console.log(name);
+
+  // TODO: prove that a name is in the list 
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if(isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
